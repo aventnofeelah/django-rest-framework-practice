26bb27fa18f0aabae0ff2d4255a08ccf0e0baf5b
avent <email@gmail.com>
2025-05-15 18:22:45 +0500

    Chat added

--- fm_project/fm_project/settings.py
+++ fm_project/fm_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_!yyqe=e&8%-$xb*jd_lqxx99)cwh#t@zi0c+@&%1z16$_n$4s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
